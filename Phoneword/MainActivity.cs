2fad510a44897cd5a78590fc82d64b100eacb493
Andre Luiz Hashiguchi <alshashiguchi@gmail.com>
2016-04-04 23:00:52 -0300

    Arquivo gitignore

---
+++ Phoneword/MainActivity.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace Phoneword
+{
+    [Activity(Label = "Phone Word", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        int count = 1;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            EditText phoneNumberText = FindViewById<EditText>(Resource.Id.PhoneNumberText);
+            Button translateButton = FindViewById<Button>(Resource.Id.TranslateButton);
+            Button callButton = FindViewById<Button>(Resource.Id.CallButton);
+
+            callButton.Enabled = false;
+
+            string translateNumber = string.Empty;
+
+            translateButton.Click += (object sender, EventArgs e) =>
+            {
+                translateNumber = Core.PhonewordTranslator.ToNumber(phoneNumberText.Text);
+
+                if (string.IsNullOrWhiteSpace(translateNumber))
+                {
+                    callButton.Text = "Call";
+                    callButton.Enabled = false;
+                }
+                else
+                {
+                    callButton.Text = "Call " + translateNumber;
+                    callButton.Enabled = true;
+                }
+            };
+
+            callButton.Click += (object sender, EventArgs e) =>
+            {
+                var callDialog = new AlertDialog.Builder(this);
+                callDialog.SetMessage("Call " + translateNumber + "?");
+                callDialog.SetNeutralButton("Call", delegate {
+                    var callIntent = new Intent(Intent.ActionCall);
+                    callIntent.SetData(Android.Net.Uri.Parse("tel:" + translateNumber));
+                    StartActivity(callIntent);
+                });
+
+                callDialog.SetNegativeButton("Cancel", delegate { });
+
+                callDialog.Show();
+            };
+        }
+    }
+}
+
